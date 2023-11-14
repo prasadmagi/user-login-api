6ead498ac4f0873fc561a73e911d218c53e118aa
prasadmagi <magiprasad@gmail.com>
2023-11-14 13:51:58 +0530

    Addes User Login Already mgs to Logout and login

--- server.js
+++ server.js
@@ -16,9 +16,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 connectDB();
 app.get("/logout", (req, res) => {
   try {
-    res.clearCookie("authcookie");
-    res.status(200).json({message:"User logout succesfully" ,msgId:0});
-    return;
+
+    let cookie = req.cookies.authcookie
+    console.log(cookie, "cookie");
+    if (cookie) {
+
+      res.clearCookie("authcookie");
+      return res.status(200).json({ message: "User logout succesfully", msgId: 0 });
+    } else {
+      return res.status(200).json({ message: "Please Login first", msgId: 0 })
+    }
+
   } catch (err) {
     console.log(err);
   }
@@ -41,7 +49,7 @@ app.post("/createUser", async (req, res) => {
   try {
     const find = await Users.findOne({ name });
     if (find) {
-      return res.status(200).json({message:"User Already Present", msgId : -1 });
+      return res.status(200).json({ message: "User Already Present", msgId: -1 });
     }
     users = new Users({
       name,
@@ -56,16 +64,16 @@ app.post("/createUser", async (req, res) => {
   }
 
   if (users) {
-    return res.status(201).json({message:"User created successfully",msgId:0});
+    return res.status(201).json({ message: "User created successfully", msgId: 0 });
   } else {
-    return res.status(404).json({message:"User not created",msgId:-1});
+    return res.status(404).json({ message: "User not created", msgId: -1 });
   }
 });
 
 app.post("/loginUser", async (req, res) => {
   const { name, password } = req.body;
   const key = process.env.Secreat_Key;
-
+  let cookie = req.cookies.authcookie
   try {
     const findUser = await Users.findOne({ name });
 
@@ -74,26 +82,28 @@ app.post("/loginUser", async (req, res) => {
       password: findUser.password,
     };
 
-    const token = jwt.sign(paylod, key, { expiresIn: "2d" });
+    console.log(cookie, "cookie");
+    if (cookie) {
+      return res.status(200).json({ message: "User Already login", msgId: 0 })
+    }
 
-    console.log(token);
-
-    res.cookie("authcookie", token, { maxAge: 900000, httpOnly: true });
 
     if (findUser && (await bcrypt.compare(password, findUser.password))) {
-     return  res.status(200).json({
+      const token = jwt.sign(paylod, key, { expiresIn: "2d" });
+      res.cookie("authcookie", token, { maxAge: 900000, httpOnly: true });
+      return res.status(200).json({
         message: "User login Successfully",
         msgId: 0,
         token: token,
       });
     } else {
-      res.status(200).json({message:"User login failed", msgId:-1});
-      return 
+      res.status(200).json({ message: "User login failed", msgId: -1 });
+      return
     }
   } catch (err) {
     console.log(err);
-   res.status(500).send("Internal Server Error");
-   return 
+    res.status(500).send("Internal Server Error");
+    return
   }
 });
 
@@ -107,9 +117,9 @@ app.get("/alluser", async (req, res) => {
   }
 
   if (users) {
-    res.status(200).json({message:"User are as follow",user:users, msgId:0});
+    res.status(200).json({ message: "User are as follow", user: users, msgId: 0 });
   } else {
-    res.status(200).json({message:"User not fetched",msgId:0});
+    res.status(200).json({ message: "User not fetched", msgId: 0 });
   }
 });
 app.get("/protect", async (req, res) => {
@@ -136,18 +146,18 @@ app.get("/protect", async (req, res) => {
   }
 });
 app.get("/", (req, res) => {
-  res.json({message:"Welcome to User Authentication App", msgId:0});
+  res.json({ message: "Welcome to User Authentication App", msgId: 0 });
 });
 // app.post("/deleteUser", async (req,res)=>{
 //   const { name, password } = req.body;
 
- 
+
 //   try {
 //     const findUser = await Users.findOne({ name });
 
- 
 
-  
+
+
 
 //     if (findUser && (await bcrypt.compare(password, findUser.password))) {
 //       // console.log("inside");
