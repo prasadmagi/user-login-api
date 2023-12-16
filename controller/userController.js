f7b363077f389ee0c5bad3d4ffe097bb90509628
prasadmagi <magiprasad@gmail.com>
2023-12-16 15:04:14 +0530

    Added userData route

--- controller/userController.js
+++ controller/userController.js
@@ -1,132 +1,133 @@
 const Users = require("../models/user");
+const UsersData = require("../models/userdata")
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
-const main = async(req,res)=>{
-    res.json({ message: "Welcome to User Authentication App", msgId: 0 });
+const main = async (req, res) => {
+  res.json({ message: "Welcome to User Authentication App", msgId: 0 });
 }
 
-const createUser = async(req,res)=>{
-    const { name, password, role } = req.body;
+const createUser = async (req, res) => {
+  const { name, password, role } = req.body;
 
-    if (!(name && password)) {
-      res.status(404).send("Please provide all value");
-      return;
-    }
-  
-    if (typeof role !== "boolean") {
-      return res.status(400).send("Please provide proper value for role !");
-    }
-    let users;
-    let securedpassword = await bcrypt.hash(password, 10);
-  
-    try {
-      const find = await Users.findOne({ name });
-      if (find) {
-        return res
-          .status(200)
-          .json({ message: "User Already Present", msgId: -1 });
-      }
-      users = new Users({
-        name,
-        password: securedpassword,
-        role: role,
-      });
-  
-      await users.save();
-    } catch (err) {
-      res.status(500).send("Internal Server Error");
-      console.log(err);
-      return;
-    }
-  
-    if (users) {
-      if (role) {
-        return res
-          .status(201)
-          .json({ message: "Admin User created successfully", msgId: 0 });
-      } else {
-        return res
-          .status(201)
-          .json({ message: "User created successfully", msgId: 0 });
-      }
-    } else {
-      return res.status(404).json({ message: "User not created", msgId: -1 });
-    }
-}
+  if (!(name && password)) {
+    res.status(404).send("Please provide all value");
+    return;
+  }
 
-const loginUser = async(req, res)=>{
-    const { name, password } = req.body;
-    const key = process.env.Secreat_Key;
-    let cookie = req.cookies.authcookie;
-    try {
-      const findUser = await Users.findOne({ name });
-      if (!findUser) {
-        return res.json({ message: "User Not Found", msgId: -1 });
-      }
-      const paylod = {
-        name: findUser.name,
-        password: findUser.password,
-      };
-  
-      console.log(cookie, "cookie");
-      if (cookie) {
-        return res.status(200).json({ message: "User Already login", msgId: 0 });
-      }
-  
-      if (findUser && (await bcrypt.compare(password, findUser.password))) {
-        const token = jwt.sign(paylod, key, { expiresIn: "2d" });
-        res.cookie("authcookie", token, { maxAge: 900000, httpOnly: true });
-        return res.status(200).json({
-          message: "User login Successfully",
-          msgId: 0,
-          token: token,
-        });
-      } else {
-        res.status(200).json({ message: "User login failed", msgId: -1 });
-        return;
-      }
-    } catch (err) {
-      console.log(err);
-      res.status(500).send("Internal Server Error");
-      return;
-    }
-}
+  if (typeof role !== "boolean") {
+    return res.status(400).send("Please provide proper value for role !");
+  }
+  let users;
+  let securedpassword = await bcrypt.hash(password, 10);
 
-const deleteUser = async(req,res)=>{
-    const { name, password } = req.body;
-    const findUser = await Users.findOne({ name });
-    try {
-      if (!(name && password)) {
-        return res
-          .status(200)
-          .json({ message: "Please provide all values..!", msgId: -1 });
-      }
-      if (findUser && (await bcrypt.compare(password, findUser.password))) {
-        Users.deleteOne({ name: findUser.name })
-          .then(function () {
-            return res
-              .status(200)
-              .json({ message: "User deleted Successfully", msgId: 0 });
-          })
-          .catch((err) => {
-            return res
-              .status(200)
-              .json({ message: "User not deleted", msgId: -1 });
-          });
-      } else {
-        return res.status(200).json({ message: "User Not Found", msgId: -1 });
-      }
-    } catch (err) {
-      console.log(err);
+  try {
+    const find = await Users.findOne({ name });
+    if (find) {
       return res
-        .status(500)
-        .json({ message: "Internal Server Error", msgId: -1 });
+        .status(200)
+        .json({ message: "User Already Present", msgId: -1 });
     }
+    users = new Users({
+      name,
+      password: securedpassword,
+      role: role,
+    });
+
+    await users.save();
+  } catch (err) {
+    res.status(500).send("Internal Server Error");
+    console.log(err);
+    return;
+  }
+
+  if (users) {
+    if (role) {
+      return res
+        .status(201)
+        .json({ message: "Admin User created successfully", msgId: 0 });
+    } else {
+      return res
+        .status(201)
+        .json({ message: "User created successfully", msgId: 0 });
+    }
+  } else {
+    return res.status(404).json({ message: "User not created", msgId: -1 });
+  }
 }
 
-const changePassword = async(req,res)=>{
-    const { name, oldpassword, newpassword } = req.body;
+const loginUser = async (req, res) => {
+  const { name, password } = req.body;
+  const key = process.env.Secreat_Key;
+  let cookie = req.cookies.authcookie;
+  try {
+    const findUser = await Users.findOne({ name });
+    if (!findUser) {
+      return res.json({ message: "User Not Found", msgId: -1 });
+    }
+    const paylod = {
+      name: findUser.name,
+      password: findUser.password,
+    };
+
+    console.log(cookie, "cookie");
+    if (cookie) {
+      return res.status(200).json({ message: "User Already login", msgId: 0 });
+    }
+
+    if (findUser && (await bcrypt.compare(password, findUser.password))) {
+      const token = jwt.sign(paylod, key, { expiresIn: "2d" });
+      res.cookie("authcookie", token, { maxAge: 900000, httpOnly: true });
+      return res.status(200).json({
+        message: "User login Successfully",
+        msgId: 0,
+        token: token,
+      });
+    } else {
+      res.status(200).json({ message: "User login failed", msgId: -1 });
+      return;
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+    return;
+  }
+}
+
+const deleteUser = async (req, res) => {
+  const { name, password } = req.body;
+  const findUser = await Users.findOne({ name });
+  try {
+    if (!(name && password)) {
+      return res
+        .status(200)
+        .json({ message: "Please provide all values..!", msgId: -1 });
+    }
+    if (findUser && (await bcrypt.compare(password, findUser.password))) {
+      Users.deleteOne({ name: findUser.name })
+        .then(function () {
+          return res
+            .status(200)
+            .json({ message: "User deleted Successfully", msgId: 0 });
+        })
+        .catch((err) => {
+          return res
+            .status(200)
+            .json({ message: "User not deleted", msgId: -1 });
+        });
+    } else {
+      return res.status(200).json({ message: "User Not Found", msgId: -1 });
+    }
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", msgId: -1 });
+  }
+}
+
+const changePassword = async (req, res) => {
+  const { name, oldpassword, newpassword } = req.body;
   const finduser = await Users.findOne({ name });
 
   try {
@@ -171,8 +172,8 @@ const changePassword = async(req,res)=>{
   }
 }
 
-const changeUserName = async (req,res)=>{
-    const { oldname, newname, password } = req.body;
+const changeUserName = async (req, res) => {
+  const { oldname, newname, password } = req.body;
   const finduser = await Users.findOne({ name: oldname });
 
   try {
@@ -217,62 +218,102 @@ const changeUserName = async (req,res)=>{
   }
 }
 
-const logout = async(req,res)=>{
-    try {
-        let cookie = req.cookies.authcookie;
-        console.log(cookie, "cookie");
-        if (cookie) {
-          res.clearCookie("authcookie");
-          return res
-            .status(200)
-            .json({ message: "User logout succesfully", msgId: 0 });
-        } else {
-          return res.status(200).json({ message: "Please Login first", msgId: 0 });
-        }
-      } catch (err) {
-        console.log(err);
-      }
-}
-
-const protect = async(req, res)=>{
-    const token = req.cookies.authcookie;
-    const key = process.env.Secreat_Key;
-    try {
-      const decode = jwt.verify(token, key);
-  
-      const name = decode.name;
-  
-      const user = await Users.findOne({ name });
-  
-      if (user) {
-        res.json({
-          msg: `Welcome user ${user.name}! and note this is proctected route`,
-        });
-      } else {
-        res.status(403).send("UnAuthorzed User");
-      }
-    } catch (err) {
-      console.log(err);
-      res.status(404).send("Invalid token");
-    }
-}
-
-const allUser = async(req,res)=>{
-    let users;
-
-    try {
-      users = await Users.find();
-    } catch (err) {
-      res.status(500).send("Internal Server Error");
-    }
-  
-    if (users) {
-      res
+const logout = async (req, res) => {
+  try {
+    let cookie = req.cookies.authcookie;
+    console.log(cookie, "cookie");
+    if (cookie) {
+      res.clearCookie("authcookie");
+      return res
         .status(200)
-        .json({ message: "User are as follow", user: users, msgId: 0 });
+        .json({ message: "User logout succesfully", msgId: 0 });
     } else {
-      res.status(200).json({ message: "User not fetched", msgId: 0 });
+      return res.status(200).json({ message: "Please Login first", msgId: 0 });
     }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const protect = async (req, res) => {
+  const token = req.cookies.authcookie;
+  const key = process.env.Secreat_Key;
+  try {
+    const decode = jwt.verify(token, key);
+
+    const name = decode.name;
+
+    const user = await Users.findOne({ name });
+
+    if (user) {
+      res.json({
+        msg: `Welcome user ${user.name}! and note this is proctected route`,
+      });
+    } else {
+      res.status(403).send("UnAuthorzed User");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(404).send("Invalid token");
+  }
+}
+
+const allUser = async (req, res) => {
+  let users;
+
+  try {
+    users = await Users.find();
+  } catch (err) {
+    res.status(500).send("Internal Server Error");
+  }
+
+  if (users) {
+    res
+      .status(200)
+      .json({ message: "User are as follow", user: users, msgId: 0 });
+  } else {
+    res.status(200).json({ message: "User not fetched", msgId: 0 });
+  }
+}
+
+const userData = async (req, res) => {
+  const { name, data } = req.body
+  const findUser = await Users.find({ name })
+  try {
+    if (!(name && data)) {
+      return res.status(200).json({ message: "Please Provide All Values", msgId: -1 })
+    }
+    if (!findUser) {
+      return res.status(200).json({ message: "User Not Found", msgId: -1 })
+    } else {
+      let finduserdata = await UsersData.findOne({ name })
+
+      if (finduserdata) {
+        let UserdataUpdated = UsersData.findOneAndUpdate({ name: name }, { data: data })
+        if (!UserdataUpdated) {
+          return res.status(200).json({ message: "User data not updated", msgId: -1 })
+        } else {
+          // console.log(msg);
+          return res.status(200).json({ message: "User data updated", msgId: 0 })
+
+        }
+
+      } else {
+        let UserDataNew = new UsersData({
+          name: name,
+          data: data
+        })
+
+        await UserDataNew.save()
+
+        return res.status(200).json({ message: "User data created", msgId: 0 })
+      }
+    }
+
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal Server Error", msgId: -1 })
+  }
 }
 exports.main = main
 exports.createUser = createUser
@@ -283,3 +324,4 @@ exports.changeUserName = changeUserName
 exports.logout = logout
 exports.protect = protect
 exports.allUser = allUser
+exports.userData = userData
--- models/userdata.js
+++ models/userdata.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = Schema({
+    name: {
+        type: String,
+        required: true
+    },
     data: {
         type: String,
         required: true
